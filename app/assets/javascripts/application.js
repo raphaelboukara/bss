503128ec51e8e2c69e825c67f8d1adb8ee3e0d2d
Raphael Boukara <raphael.boukara@gmail.com>
2014-01-27 11:01:32 +0200

    test bootstrap glyphicons files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,6 @@
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
 //= require lib/angular/angular.min
 //= require lib/angular/angular-resource.min
 //= require lib/angular/angular-route.min
